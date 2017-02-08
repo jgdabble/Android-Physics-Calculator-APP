3f611abc89fc309fef3db8b3b47574dd81a974f3
jgdabble <jgdabble@gmail.com>
2017-02-07 21:15:55 -0700

    I actually wrote some code; It's kinda important.

--- PhysicsCalculator - ANDROID/PhysicsCalculator - ANDROID/MainActivity.cs
+++ PhysicsCalculator - ANDROID/PhysicsCalculator - ANDROID/MainActivity.cs
@@ -1,4 +1,4 @@
-﻿using Android.App;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 
@@ -10,10 +10,140 @@ namespace PhysicsCalculator___ANDROID
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            bool OnEarth;
+            float mass;
+            float accel;
+            float velocity;
+            float initvelocity;
+
+            float TotalEnergy;
+            float KenEnergy;
+            float PotentialEnergy;
+            float GravPotentialEnergy;
+            float SpringPotentialEnergy;
+
+            float SpringDisplacement;
+            float SpringConstant;
+            float amplitude;
+            float period;
+            float PendulumLength;
+            // SOOOOOO MANY FLOATS!!!! THERE'S A ROOTBEER ONE, AND A ORANGE ONE, AND A SPRITE ONE, AND A BOAT!
+            float NetForce;
+            float Force;
+            float GravForce;
+            float FrictionForce;
+            float SpringForce;
+
+            float GravAccel;
+            float SatMass;
+            float PlanMass;
+            float OrbitalGravForce;
+            float OrvitalVelocity;
+            float EscapeVelocity;
+            float UniGravConst = 0.0000000000667408;
+
+            // Is the calculation taking place on earth or some other place in the vast cosmos?
+            ToggleButton EarthGrav = FindViewById<ToggleButton>(Resource.Id.EarthGravToggle);
+            EarthGrav.Click += (o, e) =>
+            {
+                if (EarthGrav.Checked)
+                    OnEarth == true;
+
+                else
+                    OnEarth == false;
+            };
+
+            // Is there an active spring working in the problem?
+            ToggleButton SpringsToggle = FindViewById<ToggleButton>(Resource.Id.SpringToggle);
+            SpringsToggle.Click += (o, e) =>
+            {
+                if (SpringsToggle.Checked)
+                    SpringsOn == true;
+
+                else
+                    SpringsOn == false;
+            };
+
+            // Is there a swinging pendulum?
+            ToggleButton PendulumsToggle = FindViewById<ToggleButton>(Resource.Id.PendulumToggle);
+            PendulumsToggle.Click += (o, e) =>
+            {
+                if (PendulumsToggle.Checked)
+                    PendulumsOn == true;
+
+                else
+                    PendulumsOn == false;
+            };
+            
+            // Is the object moving in circular or non-circular motion?
+            ToggleButton MotionType = FindViewById<ToggleButton>(Resource.Id.CircularMotionToggle);
+            MotionType.Click += (o, e) =>
+            {
+                if (MotionType.Checked)
+                    CircularMotion == false;
+
+                else
+                    CircularMotion == true;
+            };
+            
+            // What is the mass of the main object?
+            CheckBox mass = FindViewById<CheckBox>(Resource.Id.MassCheck);
+            mass.Click += (o, e) => {
+                if (mass.Checked) ;
+                    // Stuff goes here!
+                else;
+                    // Stuff goes here!
+            };
+
+            CheckBox LinearAcceleration = FindViewById<CheckBox>(Resource.Id.AccelerationCheck);
+            LinearAcceleration.Click += (o, e) => {
+                if (LinearAcceleration.Checked) ;
+                // Stuff goes here!
+                else;
+                // Stuff goes here!
+            };
+
+            CheckBox LinearVelocity = FindViewById<CheckBox>(Resource.Id.VelocityCheck);
+            LinearVelocity.Click += (o, e) => {
+                if (LinearVelocity.Checked) ;
+                // Stuff goes here!
+                else;
+                // Stuff goes here!
+            };
+
+            CheckBox InitialVelocityCheck = FindViewById<CheckBox>(Resource.Id.InitialVelocityCheck);
+            checkbox.Click += (o, e) => {
+                if (checkbox.Checked) ;
+                // Stuff goes here!
+                else;
+                // Stuff goes here!
+            };
+
+            CheckBox checkbox = FindViewById<CheckBox>(Resource.Id.checkbox);
+            checkbox.Click += (o, e) => {
+                if (checkbox.Checked) ;
+                // Stuff goes here!
+                else;
+                // Stuff goes here!
+            };
+
+            CheckBox checkbox = FindViewById<CheckBox>(Resource.Id.checkbox);
+            checkbox.Click += (o, e) => {
+                if (checkbox.Checked) ;
+                // Stuff goes here!
+                else;
+                // Stuff goes here!
+            };
+
+            CheckBox checkbox = FindViewById<CheckBox>(Resource.Id.checkbox);
+            checkbox.Click += (o, e) => {
+                if (checkbox.Checked) ;
+                // Stuff goes here!
+                else;
+                // Stuff goes here!
+            };
         }
     }
 }
-
